a28d16d72a1449d55820c06bfc45d15e78f317b2
Bostan Victor <victorbostan01@gmail.com>
2024-10-05 17:06:02 +0300

    Complete lab1 task3

--- lab1.py
+++ lab1.py
@@ -25,6 +25,25 @@ if response.status_code == 200:
         price = price_container.find('b').text if price_container else "Price not found" 
         print(f"Product Name: {name}")
         print(f"Product Price: {price}")
+
+        # Task 3 (Extract product link and something from product page)
+        product_link_tag = product.find('a', class_='d-block mb-2 ga-item') 
+        product_link = product_link_tag['href'] if product_link_tag else "Link not found"
+        print(f"Product Link: {product_link}")
+
+        product_response = requests.get(product_link)
+
+        if product_response.status_code == 200:
+            product_soup = BeautifulSoup(product_response.text, 'html.parser')
+
+            specs_list_tag = product_soup.find('ul', class_='features')
+            specs_list = specs_list_tag.find_all('li', class_='char_all')
+            specs = [spec.text for spec in specs_list]
+            print("Device specs:")
+            for spec in specs:
+                print(spec)
+        else:
+            print(f"Failed to retrieve the product page. Status Code: {product_response.status_code}\n")
         print("-------")
 else:
     print("Failed to retrieve the website. Status Code:", response.status_code)
