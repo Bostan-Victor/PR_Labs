85802777bc18fabfeaa80f67c31459c4a313a1a5
Bostan Victor <vain6677@gmail.com>
2024-11-14 00:19:53 +0200

    Complete tasks 7 and 8

--- Lab2/app.py
+++ Lab2/app.py
@@ -6,7 +6,8 @@ import websockets
 import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@db:5432/mydatabase'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
